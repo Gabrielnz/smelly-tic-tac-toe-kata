3e81607d8904b1f2044ee03564decb617b8f9c27
gnunez <gnunez@aidacanarias.com>
2022-02-16 08:48:05 +0000

    renaming field

--- TypeScript/src/Board.ts
+++ TypeScript/src/Board.ts
@@ -1,14 +1,14 @@
 import { Player, Position } from "./Tile";
 
 export class Board {
-    private readonly _newPlays: Player[] = [];
+    private readonly _plays: Player[] = [];
 
     constructor() {
-        this._newPlays = new Array(9).fill(Player.None);
+        this._plays = new Array(9).fill(Player.None);
     }
 
     public TileAt2(position: Position): Player {
-        return this._newPlays[position];
+        return this._plays[position];
     }
 
     public TileHasPlayer2(player: Player, position: Position): Boolean {
@@ -16,13 +16,13 @@ export class Board {
     }
 
     public AddTileAt2(player: Player, position: Position): void {
-        this._newPlays[position] = player;
+        this._plays[position] = player;
     }
 
     public AnyRowIsTakenBy2(player: Player): Boolean {
-        const topRow = this._newPlays[Position.TopLeft] === player && this._newPlays[Position.TopMiddle] === player && this._newPlays[Position.TopRight] === player;
-        const middleRow = this._newPlays[Position.MiddleLeft] === player && this._newPlays[Position.Middle] === player && this._newPlays[Position.MiddleRight] === player;
-        const bottomRow = this._newPlays[Position.BottomLeft] === player && this._newPlays[Position.BottomMiddle] === player && this._newPlays[Position.BottomRight] === player;
+        const topRow = this._plays[Position.TopLeft] === player && this._plays[Position.TopMiddle] === player && this._plays[Position.TopRight] === player;
+        const middleRow = this._plays[Position.MiddleLeft] === player && this._plays[Position.Middle] === player && this._plays[Position.MiddleRight] === player;
+        const bottomRow = this._plays[Position.BottomLeft] === player && this._plays[Position.BottomMiddle] === player && this._plays[Position.BottomRight] === player;
         return topRow || middleRow || bottomRow;
     }
 
